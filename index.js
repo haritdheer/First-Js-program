531c8744f501d40d9abda13473f459a579cdf7d8
haritdheer <haritdheer@gmail.com>
2022-02-08 23:21:22 +0530

    Backticks in js

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ console.log(typeof a);
 a=null;
 console.log(a);
 console.log(typeof a);
+
+var b="How are you.\n I m fine"
+console.log(b);
+
+var str=`aye aye aye
+oh oh oh`
+console.log(str);
+
+var num=212;
+console.log(`Half of ${num} is ${num/2}`);
