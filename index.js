d61ec76ff1e404e55f2f934f4f6974ce82020cd4
haritdheer <haritdheer@gmail.com>
2022-02-08 23:05:55 +0530

    first js program

---
+++ index.js
@@ -0,0 +1,16 @@
+// variable declaration
+var a=10;
+console.log(a);
+console.log(typeof a);
+
+a="Harit";
+console.log(a);
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a=null;
+console.log(a);
+console.log(typeof a);
