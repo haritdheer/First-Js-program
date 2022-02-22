5e94d5fbb436ac790c28d10fb34ef03f7e0b8ac8
haritdheer <haritdheer@gmail.com>
2022-02-22 22:32:11 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,7 @@
+console.log(x);
+greet ();
+var x=10;
+function greet(){
+    console.log("Hello");
+}
+//Javascript hoisting refers to the process whereby the interpreter appears to move the declaration of function ,variables to the top of their scope prior to the execution of the scope.//
