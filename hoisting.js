1f41f042267fbf5013fb3d09f871a614a7e0e4a5
haritdheer <haritdheer@gmail.com>
2022-02-23 00:04:17 +0530

    scopes and lexical env explained

--- hoisting.js
+++ hoisting.js
@@ -1,7 +1,14 @@
 console.log(x);
 greet ();
+hello ();
 var x=10;
 function greet(){
     console.log("Hello");
 }
-//Javascript hoisting refers to the process whereby the interpreter appears to move the declaration of function ,variables to the top of their scope prior to the execution of the scope.//
+//Javascript hoisting refers to the process whereby the interpreter appears to move the declaration of function ,variables to the top of their scope prior to the execution of the scope.//ve initialised its value
+
+//definition
+// we can acess the variables even before we have initialised its value
+var hello=function(){
+    console.log("u guys rock");
+}
