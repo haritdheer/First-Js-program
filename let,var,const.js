7c4ef534ac06654716510c5b198d7bd9fb0189aa
haritdheer <haritdheer@gmail.com>
2022-03-03 23:08:02 +0530

    hoisting with example

--- let,var,const.js
+++ let,var,const.js
@@ -1,4 +1,15 @@
-console.log(x);
-var x = 10;
-let b = 100;
+// console.log(x);
+// var x = 10;
+// let b = 100;
+// console.log(b);
+
+let b=10;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
 console.log(b);
